b0518c2e0aca18f2184eb5e38f7c8655053f6da0
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-13 18:34:58 -0400

    Update `op` to v0.5.7

--- Formula/op.rb
+++ Formula/op.rb
@@ -2,10 +2,14 @@ class Op < Formula
   desc "1Password CLI"
   homepage "https://app-updates.agilebits.com/product_history/CLI"
 
-  url "https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_darwin_amd64_v0.5.5.zip"
-  sha256 "e4ea329debcf991434d90728fa3cba531bce5449a08883d3530dfeb796fc3a3b"
+  url "https://cache.agilebits.com/dist/1P/op/pkg/v0.5.7/op_darwin_amd64_v0.5.7.zip"
+  sha256 "f382de2e5682c8f6a935123a9401cb77db6680233c80c5e9118cfa70b3f1959a"
 
   def install
     bin.install "op"
+  end
+
+  test do
+    system "#{bin}/op"
   end
 end
