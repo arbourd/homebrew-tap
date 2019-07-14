f8415ba6c20d439acc7e8619f6d5b1be7946d16f
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-13 22:16:31 -0400

    Update coco-cpp to 20181203
    Previous version was 2010XXXX

--- Formula/coco-cpp.rb
+++ Formula/coco-cpp.rb
@@ -1,14 +1,22 @@
 class CocoCpp < Formula
   desc "The Compiler Generator Coco/R - C++ version"
-  homepage "https://github.com/olesenm/coco-cpp"
-  head "https://github.com/olesenm/coco-cpp.git"
+  homepage "http://www.ssw.uni-linz.ac.at/coco/"
+
+  url "http://www.ssw.uni-linz.ac.at/coco/CPP/CocoSourcesCPP.zip"
+  sha256 "fa968179d063b3ee6a0047ca70f5948f2f572f82a9194731ad0252ff8c1f7223"
+  version "20181203"
 
   depends_on :xcode => ["6.0", :build]
 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make"
-    system "make", "install"
+    system "chmod", "+x", "./build.sh"
+    system "./build.sh"
+    bin.install "Coco" => "coco-cpp"
+    pkgshare.install "Scanner.frame", "Parser.frame"
+  end
+
+  def caveats
+    "Frame files have been installed to #{pkgshare}."
   end
 
   test do
