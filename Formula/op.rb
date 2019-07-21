ec66bda094621e11a0e7784f164d606d9c3c0903
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-21 18:03:41 -0400

    Make op bottless

--- Formula/op.rb
+++ Formula/op.rb
@@ -4,6 +4,8 @@ class Op < Formula
   url "https://cache.agilebits.com/dist/1P/op/pkg/v0.5.7/op_darwin_amd64_v0.5.7.zip"
   sha256 "f382de2e5682c8f6a935123a9401cb77db6680233c80c5e9118cfa70b3f1959a"
 
+  bottle :unneeded
+
   def install
     bin.install "op"
   end
