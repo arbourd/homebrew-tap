232274562a9a9e314e1076b2512c48045c9744f4
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-20 22:42:20 -0400

    Add Travis

--- Formula/png2icns.rb
+++ Formula/png2icns.rb
@@ -1,7 +1,6 @@
 class Png2icns < Formula
   desc "Converts png to icns"
   homepage "https://github.com/bitboss-ca/png2icns"
-
   url "https://github.com/bitboss-ca/png2icns/archive/v0.1.tar.gz"
   sha256 "adb9726483770dac9f9945e791223e1977aeb008481ea5dd80cdaaedd1bb3714"
   head "https://github.com/bitboss-ca/png2icns.git"
