8859aa92fdbe8c40d94cf7549e9057b25715dc99
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-24 22:59:17 -0400

    git-get 0.1.1

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -1,8 +1,8 @@
 class GitGet < Formula
   desc "Go gets your code"
   homepage "https://github.com/arbourd/git-get"
-  url "https://github.com/arbourd/git-get/archive/v0.1.0.tar.gz"
-  sha256 "0161e05325acca225db304ba37beba891ccd4e02536f1c1490ee44ac0f4a48d4"
+  url "https://github.com/arbourd/trashOS/archive/v0.1.1.tar.gz"
+  sha256 "1b2049fe81142bd258a00eaed35ed7de9208f45c3059a61f6266c420e969c002"
   head "https://github.com/arbourd/git-get.git"
 
   bottle do
