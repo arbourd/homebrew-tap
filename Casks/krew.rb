849dd431c84def4659b3884d05495cf4df15a765
Dylan Arbour <dylan.arbour@gmail.com>
2019-07-24 23:51:30 -0400

    Update CI
    - Adds style checking to casks and fixes violations
    - Uses a Makefile over a shell script for test running

--- Casks/krew.rb
+++ Casks/krew.rb
@@ -2,14 +2,15 @@ cask 'krew' do
   version :latest
   sha256 :no_check
 
+  # storage.googleapis.com was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/krew/v0.2.1/krew.tar.gz'
   name 'krew'
-  homepage 'https://krew.dev'
+  homepage 'https://krew.dev/'
 
   installer script: {
-    executable: 'krew-darwin_amd64',
-    args:       ['install', 'krew'],
-  }
+                      executable: 'krew-darwin_amd64',
+                      args:       ['install', 'krew'],
+                    }
 
   uninstall delete: ['~/.krew/bin/kubectl-krew', '~/.krew/store/krew']
 
