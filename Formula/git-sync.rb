dfbe9dc0a87831711a607bb669fcb3ba1fd326f1
Dylan Arbour <arbourd@users.noreply.github.com>
2020-12-23 23:33:38 -0500

    Fix spacing

--- Formula/git-sync.rb
+++ Formula/git-sync.rb
@@ -1,22 +1,22 @@
 class GitSync < Formula
-    desc "Updates your branches"
-    homepage "https://github.com/arbourd/git-sync"
-    url "https://github.com/arbourd/git-sync/archive/v0.0.0.tar.gz"
-    sha256 "892c39068d89c5029e08d8f8ebfbe1a268552bcafd6ac189786505cd231bd4cb"
-    head "https://github.com/arbourd/git-sync.git"
+  desc "Updates your branches"
+  homepage "https://github.com/arbourd/git-sync"
+  url "https://github.com/arbourd/git-sync/archive/v0.0.0.tar.gz"
+  sha256 "892c39068d89c5029e08d8f8ebfbe1a268552bcafd6ac189786505cd231bd4cb"
+  head "https://github.com/arbourd/git-sync.git"
 
-    depends_on "go" => :build
+  depends_on "go" => :build
 
-    def install
-      ENV["GOPATH"] = buildpath
-      ENV["GO111MODULE"] = "on"
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
 
-      dir = buildpath/"src/github.com/arbourd/git-sync"
-      dir.install buildpath.children
+    dir = buildpath/"src/github.com/arbourd/git-sync"
+    dir.install buildpath.children
 
-      cd dir do
-        system "go", "build", "-o", bin/"git-sync"
-        prefix.install_metafiles
-      end
+    cd dir do
+      system "go", "build", "-o", bin/"git-sync"
+      prefix.install_metafiles
     end
+  end
   end
