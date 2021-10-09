0fc851d1275b75d99b14437b994b6585485ebe7a
Dylan Arbour <arbourd@users.noreply.github.com>
2021-10-09 15:26:36 -0400

    git-sync 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/git-sync.rb
+++ Formula/git-sync.rb
@@ -1,8 +1,8 @@
 class GitSync < Formula
   desc "Updates your branches"
   homepage "https://github.com/arbourd/git-sync"
-  url "https://github.com/arbourd/git-sync/archive/v0.1.0.tar.gz"
-  sha256 "e9c48ce5ddf8ac99a1c241177cdc8570b7fb96441b73bf218aba4925ab73fa8f"
+  url "https://github.com/arbourd/git-sync/archive/v0.1.1.tar.gz"
+  sha256 "486a066db679bc434d3d817a59de95929e25e52c18b25ce457e4b6c52a1ee1be"
   head "https://github.com/arbourd/git-sync.git"
 
   depends_on "go" => :build
