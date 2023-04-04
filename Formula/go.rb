b47d250b6858090592960958084be1855d25f063
arbourd <arbourd@users.noreply.github.com>
2023-04-04 18:05:54 +0000

    Go v1.20.3

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,31 +1,31 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  version "1.20.2"
+  version "1.20.3"
 
   if OS.mac?
-    url "https://go.dev/dl/go1.20.2.darwin-amd64.tar.gz"
-    sha256 "c93b8ced9517d07e1cd4c362c6e2d5242cb139e29b417a328fbf19aded08764c"
+    url "https://go.dev/dl/go1.20.3.darwin-amd64.tar.gz"
+    sha256 "c1e1161d6d859deb576e6cfabeb40e3d042ceb1c6f444f617c3c9d76269c3565"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://go.dev/dl/go1.20.2.darwin-arm64.tar.gz"
-    sha256 "7343c87f19e79c0063532e82e1c4d6f42175a32d99f7a4d15e658e88bf97f885"
+    url "https://go.dev/dl/go1.20.3.darwin-arm64.tar.gz"
+    sha256 "86b0ed0f2b2df50fa8036eea875d1cf2d76cefdacf247c44639a1464b7e36b95"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://go.dev/dl/go1.20.2.linux-amd64.tar.gz"
-    sha256 "4eaea32f59cde4dc635fbc42161031d13e1c780b87097f4b4234cfce671f1768"
+    url "https://go.dev/dl/go1.20.3.linux-amd64.tar.gz"
+    sha256 "979694c2c25c735755bf26f4f45e19e64e4811d661dd07b8c010f7a8e18adfca"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://go.dev/dl/go1.20.2.linux-arm64.tar.gz"
-    sha256 "78d632915bb75e9a6356a47a42625fd1a785c83a64a643fedd8f61e31b1b3bef"
+    url "https://go.dev/dl/go1.20.3.linux-arm64.tar.gz"
+    sha256 "eb186529f13f901e7a2c4438a05c2cd90d74706aaa0a888469b2a4a617b6ee54"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://go.dev/dl/go1.20.2.linux-armv6l.tar.gz"
-    sha256 "d79d56bafd6b52b8d8cbe3f8e967caaac5383a23d7a4fa9ac0e89778cd16a076"
+    url "https://go.dev/dl/go1.20.3.linux-armv6l.tar.gz"
+    sha256 "b421e90469a83671641f81b6e20df6500f033e9523e89cbe7b7223704dd1035c"
   end
 
   def install
